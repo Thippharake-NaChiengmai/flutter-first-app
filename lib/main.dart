6b59b458dd1b534d6dae6ccc52a36ac62f8316f4
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-12-16 10:25:27 +0700

    connect /Pages with main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firstapp/pages/home.dart';
+import 'package:firstapp/pages/contract.dart';
+import 'package:firstapp/pages/calculate.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,8 +34,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  int _currentIndex = 0;
+  final tabs =[HomePage(),CalculatePage(),ContractPage()];
+
   @override
   Widget build(BuildContext context) {
-    return Text("Hello World");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+          body: tabs[_currentIndex], bottomNavigationBar:
+          BottomNavigationBar(currentIndex: _currentIndex,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.contact_mail),label: 'Contract'),
+              BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.calculate),label: 'Calculate')
+            ],),
+    );
   }
 }
