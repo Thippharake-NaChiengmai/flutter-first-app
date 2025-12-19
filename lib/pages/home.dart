af95be434e42d264676f8355ad1ea6e0116e84be
Thippharake-NaChiengmai <thippharake_na@cmu.ac.th>
2025-12-19 11:47:30 +0700

    make a function for Mybox()

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,35 +10,49 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-      myBox(),
-      SizedBox(height: 10),
-      myBox(),
-      SizedBox(height: 10),
-      myBox()
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Computer & App Development Knowledge.",style: TextStyle(fontSize:15)),
+      ),
+        body: Padding(
+            padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            myBox("What is a Computer?",
+                  "Computer is an electronic device that processes data and performs a wide range of tasks by following a set of instructions, typically referred to as programs or software. It can execute complex calculations, store and retrieve data, and perform tasks like word processing, gaming, web browsing, data analysis, and much more."),
+            SizedBox(height: 20),
+            myBox("What is a Flutter?",
+                  "Flutter is an open-source UI software development kit (SDK) created by Google. It allows developers to build natively compiled applications for mobile, web, desktop, and embedded devices from a single codebase."),
+            SizedBox(height: 20),
+            myBox("What is a Dart?",
+                  "Dart is a client-optimized, object-oriented programming language created by Google. While it can be used for backend (server) development, it is most famous today for being the programming language used to build Flutter applications."),
+          ],
+          )
+        )
     );
   }
-  Widget myBox() {
+  Widget myBox(String title, String subTitle) {
     return Container(
       padding: EdgeInsets.all(24),
-      height: 250,
+      height: 275,
       decoration: BoxDecoration(
         color: Colors.deepPurpleAccent,
         borderRadius: BorderRadius.circular(24),
         image: DecorationImage(image:NetworkImage("https://i.pinimg.com/736x/67/56/95/6756956ec2e5b6b569b23110696a4ee7.jpg"),
-            fit: BoxFit.cover),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken)
+        ),
 
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("What is a Computer" ,style: TextStyle(fontSize: 25, color: Colors.white)),
-          Text("Computer is an electronic device that processes data and performs a wide range of tasks by following a set of instructions, typically referred to as programs or software. It can execute complex calculations, store and retrieve data, and perform tasks like word processing, gaming, web browsing, data analysis, and much more."
-              ,style: TextStyle(fontSize: 15, color: Colors.white))
-    ],),
+          Text(title ,style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold)),
+          SizedBox(height: 20),
+          Text(subTitle ,style: TextStyle(fontSize: 15, color: Colors.white))
+    ],
+      ),
         );
 
   }
